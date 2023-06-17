f9b05c9f1ce3b863ea1ee3f0c0304722c0f56050
solomonadola <solomonadola84@gmail.com>
2023-06-17 00:28:02 -0700

    Last digit of random number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,11 +14,12 @@ int main(void)
     srand(time(0));
     n = rand() - RAND_MAX / 2;
     digit = n % 10;
-    if(digit > 5)
+
+    if (digit > 5)
     {
         printf("Last digit of %d is %d and is greater than 5\n", n, digit);
     }
-    else if(digit == 0)
+    else if (digit == 0)
     {
         printf("Last digit of %d is %d and is 0\n", n, digit);
     }
@@ -26,6 +27,7 @@ int main(void)
     {
         printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);
     }
+
     return (0);
 }
 
