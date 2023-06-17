6367126d7083694b8ddf48db67772e262211226f
solomonadola <solomonadola84@gmail.com>
2023-06-17 00:50:42 -0700

    chxs

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the lowercase alphabet in reverse,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
