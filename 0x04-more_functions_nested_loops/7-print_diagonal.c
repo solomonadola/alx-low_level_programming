d4384aa3943d1764d02ce7623fc068186aa1764f
solomonadola <solomonadola84@gmail.com>
2023-06-22 13:05:07 -0700

    7 fixed

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,7 +2,7 @@
 
 /**
  * print_diagonal - draws a diagonal line on the terminal
- * @n: number of times the character 
+ * @n: number of times the character / printed 
  */
 void print_diagonal(int n)
 {
