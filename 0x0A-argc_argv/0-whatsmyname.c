853a779f61829cf5396de968f68d1b7b3a57ca0f
solomonadola <solomonadola84@gmail.com>
2023-07-12 13:15:49 -0700

    My name fixed

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - prints its name, followed by a new line.
  * @argc: number of command line arguments.
@@ -8,6 +7,6 @@
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
-       	printf ("%s\n",argv[0]);
-	return 0;
+	printf("%s\n", argv[0]);
+	return (0);
 }
