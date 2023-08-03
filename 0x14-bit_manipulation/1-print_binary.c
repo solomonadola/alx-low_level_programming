e4c8c3a78ef70030db6ff6bf09eaf656b3ae2488
solomonadola <solomonadola84@gmail.com>
2023-08-03 13:00:46 -0700

    Second Task complete

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_binary - a function that returns 
+ * the value of a bit at a given index
+ * of a number.
+ * @n: unsigned long int.
+ *
+ * Return: no return.
+ */
+void print_binary(unsigned long int n)
+{
+	if (n >> 0)
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
+	{
+		_putchar('0');
+	}
+}
