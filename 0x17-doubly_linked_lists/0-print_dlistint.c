d9a1a6e89d194603ba6c53d3a7a5f303deb9e658
solomonadola <solomonadola84@gmail.com>
2023-10-12 07:14:48 -0700

    First task

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,12 +11,12 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	const dlistint_t *current = h;
 	size_t count = 0;
-	
+
 	while (current != NULL)
 	{
 		printf("%d\n", current->n);
 		current = current->next;
 		count++;
 	}
-	return count;
+	return (count);
 }
