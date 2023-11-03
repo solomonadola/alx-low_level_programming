abfa8aab1ad1ed7a1ab8048edfb457c027fccbc0
solomonadola <solomonadola84@gmail.com>
2023-11-03 12:27:26 -0700

    Task 2

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -10,7 +10,7 @@
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int hash_value;
-	
+
 	if (key == NULL || size == 0)
 		return (0);
 	hash_value = hash_djb2(key);
